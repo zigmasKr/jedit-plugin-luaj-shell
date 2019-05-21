9fd9d69c80441ef20625c029a3f1a4d2b054e033
zigmasKr <zigmas.kr@gmail.com>
2019-05-21 19:45:54 +0300

    New actions, and new possibilities (around = ) in REPL and block evaluation.

--- src/luaj/shell/LuaJShell.java
+++ src/luaj/shell/LuaJShell.java
@@ -17,6 +17,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Hashtable;
 import javax.swing.text.AttributeSet;
@@ -54,19 +55,17 @@ public class LuaJShell extends ProcessShell {
 	 */
 	protected void init(ConsoleState state, String command) throws IOException {
 		String luajJar = jEdit.getProperty("options.luaj.luaj-core-path");
-
 		List<String> cmdLineLuaJ = new ArrayList<String>();
 		cmdLineLuaJ.add("java");
 		cmdLineLuaJ.add("-classpath");
 		cmdLineLuaJ.add(luajJar);
 		cmdLineLuaJ.add("lua");
-
 		ProcessBuilder pbLuaJ = new ProcessBuilder(cmdLineLuaJ);
 		state.p = pbLuaJ.start();
 		Log.log(Log.DEBUG, this, "LuaJ started.");
 	}
 	//}}}
-
+	
 	//{{{ evalCode()
 	/**
 	 * Evaluate a code (selection)
@@ -74,7 +73,9 @@ public class LuaJShell extends ProcessShell {
 	public void evalCode(Console console, String code) {
 		code = stripLuaComments(code);
 		code = code.replace('\n', ' ');
+		code = startsEquality(code);
 		send(console, code);
+		// look for send in ProcessShell
 	} //}}}
 
 	//{{{ evalBuffer()
@@ -97,6 +98,7 @@ public class LuaJShell extends ProcessShell {
 		send(console, sendString);
 	} //}}}
 
+	//{{{ onRead(...
 	protected void onRead(ConsoleState state, String str, Output output) {
 		if (str.indexOf("\n") != -1) {
 			str = str.substring(str.lastIndexOf("\n") + 1);
@@ -105,8 +107,52 @@ public class LuaJShell extends ProcessShell {
 			state.waiting = false;
 			output.commandDone();
 		}
-	}
+	} // }}}
 
+	
+	// {{{ send; overrides that from ProcessShell
+	/**
+	 * Sends input to the running process
+	 * If no process is running (e.g. the process is not a REPL), start it first
+	 */
+	protected void send(final Console console, String str, boolean fromConsole) {
+		final ConsoleState state = consoleStateMap.get(console);
+		if (!state.running) {
+			if (!start(console, console.getOutput(), str)) {
+				return;
+			}
+			if (state.p == null) {
+				console.getOutput().print(console.getErrorColor(),
+						jEdit.getProperty("msg.procshell.no-process"));
+				return;
+			}
+		}
+		try {
+			final String cmd = onWrite(state, str)+"\n";
+			if (!fromConsole) {
+				if (str.indexOf("\n") != -1) {
+					console.getOutput().print(console.getInfoColor(), "...");
+				} else {
+					console.getOutput().print(console.getInfoColor(), str);
+				}
+				//output.print(console.getInfoColor(), str);
+			}
+			new Thread() {
+				public void run() {
+					try {
+						state.write(cmd);
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+						console.getOutput().print(console.getErrorColor(),
+								jEdit.getProperty("msg.procshell.no-process"));
+					}
+				}
+			}.start();
+		} catch (Exception e) {}
+	} //}}}
+	
+	// {{{ stripLuaComments
 	protected String stripLuaComments(String code) {
 		String rgxBlockComment = "\\-\\-\\[\\[.*?\\]\\]";
 		String rgxLineComment = "\\-\\-.*?\\n";
@@ -115,12 +161,24 @@ public class LuaJShell extends ProcessShell {
 		code = ptnBlockComment.matcher(code).replaceAll(" ");
 		code = ptnLineComment.matcher(code).replaceAll(" ");
 		return code;
-	}
+	} //}}}
+	
+	// {{{ startsEquality
+	protected String startsEquality(String code) {
+		String rgEq = "^(\\s*\\=\\s*)(.*)";
+		Pattern ptEq = Pattern.compile(rgEq, Pattern.DOTALL);
+		Matcher mhEq = ptEq.matcher(code);
+		if (mhEq.find()) {
+			code = "print(" + mhEq.group(2) + ")";
+		}
+		return code;
+	} // }}}
 
+	// {{{ printInfoMessage
 	public void printInfoMessage(Output output) {
 		output.print(null, jEdit.getProperty("msg.luajshell.info-message"));
-	}
-
+	} // }}}
+	
 }
 
 /* :folding=explicit:collapseFolds=1:tabSize=4:indentSize=4:noTabs=false: */
